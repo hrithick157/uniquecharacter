2581b86ccff3f94480c00bc0a74a32819b293e9d
hrithick157 <77242622+hrithick157@users.noreply.github.com>
2021-01-14 10:42:30 +0530

    Add files via upload

---
+++ uniquechar.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main() 
+{
+    int i,m,n,cnt,len;
+	char str[500],unq[500]={0};
+	printf("Enter String:");
+	gets(str);
+	len=strlen(str);
+	m=0;
+    for(i=0;i<len;i++)
+    {
+    	cnt=0;
+    	for(n=0;n<i;n++)
+    	{
+    	   if(str[i]==unq[n])
+    	     {
+    		   cnt++;
+		     }
+	    }
+		if(cnt==0)
+		   {
+			unq[m++]=str[i];
+		   }  
+	}
+	printf("The Unique Characters in String:");
+	puts(unq);
+	getch();	
+}
